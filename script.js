38a12c73bca34b6e1b62c63a8ecadc297d35b90b
Marcos Willian <willian.marcos7@hotmail.com>
2022-04-07 14:37:34 -0300

    Arquivo JS adicionado (teste)

---
+++ script.js
@@ -0,0 +1,3 @@
+var nome=prompt("Qual é o seu nome?? ")
+
+alert("Olá, "+nome+"! Seja bem vinda ao site!! Ainda faltam alguns ajustes e estou terminando de desenvolver a pagina, volte em poucos dias e tudo estará nos conformes! ^-^")
