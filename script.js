49b196daa24abb0d0ef6f3709fda5b1253671fdb
Marcos Willian <willian.marcos7@hotmail.com>
2022-05-29 05:55:06 -0300

    Projeto quase pronto

--- script.js
+++
@@ -1,5 +0,0 @@
-var nome=prompt("Oii, qual é o seu nome?? ")
-
-alert("Olá, "+nome+"! Seja bem vinda ao site!! Ainda faltam alguns ajustes e estou terminando de desenvolver a pagina, volte em poucos dias e tudo estará nos conformes! ^-^")
-
-alert(nome+", nós já estamos quase finalizando a página, a estrutura já foi construída e estilizada, os textos são alternativos, mas já vão ser substituídos e formatados")
